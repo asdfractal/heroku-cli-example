ac74fdbecb2c25e0878c50bb6d0bdcb2aedf06b9
James Gregory <james@asdfractal.com>
2022-04-27 02:46:33 +0000

    set up app and model

--- heroku_cli/settings.py
+++ heroku_cli/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-e3hf67wp&ch8z8xe3qp$1)z%$)^%&oz#hecxp8x!)g!cq7aab_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['heroku-cli-example.herokuapp.com']
+ALLOWED_HOSTS = ['heroku-cli-example.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'myapp',
 ]
 
 MIDDLEWARE = [
@@ -76,13 +77,17 @@ WSGI_APPLICATION = 'heroku_cli.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    "default": dj_database_url.parse(os.getenv("DATABASE_URL"))
-    # "default": {
-    #     "ENGINE": "django.db.backends.sqlite3",
-    #     "NAME": BASE_DIR / "db.sqlite3",
-    # }
-}
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        "default": dj_database_url.parse(os.getenv("DATABASE_URL"))
+    }
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 
 # Password validation
